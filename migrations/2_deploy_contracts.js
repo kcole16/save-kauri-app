4154ebde047e8cb2a9365454721021d284d2c12d
Kendall Cole <kcole16@gmail.com>
2018-09-18 11:25:44 +0100

    Deploy to rinkeby

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,5 +2,5 @@ var web3 = require('web3');
 var Token = artifacts.require("./Token.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Token, "KendallCoin", "KRC", web3.utils.toWei("0.1"));
+  deployer.deploy(Token, "KauriSeedling", "KRI", web3.utils.toWei("0.1"));
 };
