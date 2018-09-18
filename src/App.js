c3bfd77f22cebce615f401a5e4252445d7e363d6
Kendall Cole <kcole16@gmail.com>
2018-09-18 11:46:11 +0100

    Eject and add contract flexibility

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     super()
     this.handleUpdate = this.handleUpdate.bind(this)
     this.state = {
-      isCompleted: false,
+      isCompleted: true,
       isPending: false,
       txHash: ''
     }
--- src/Map.js
+++ src/Map.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactMapGL, {Marker} from 'react-map-gl';
 import marker from './marker.png';
 import markerBlack from './marker-black.png';
+import TokenContract from '../build/contracts/Token.json';
 
 process.env.MapboxAccessToken = 'pk.eyJ1Ijoia2NvbGUxNiIsImEiOiJjaWc5ZmNoNzEwaW9hdmZsd2lqdTdnMHhpIn0.IgKHGTa-LjzdHd8Y23WzYg'
 
@@ -20,13 +21,14 @@ export default class Map extends React.Component {
   }
 
   render() {
+    const url = 'https://rinkeby.opensea.io/assets/'+TokenContract['networks'][4]['address']+'/1'
     return (
       <div>
           <div style={{textAlign: 'center'}}>
               <h1 className="App-title">Thank you for your contribution</h1>
               <p>Your Kauri seedling will be planted at the marker below</p>
               <p>Latitude: -37.120, Longitude: 175.629762</p>
-              <p>View your Token on OpenSea here:</p>
+              <p>View your Token on OpenSea <a href={url} target="_blank">here</a></p>
             </div>
           <ReactMapGL
             {...this.state.viewport}
